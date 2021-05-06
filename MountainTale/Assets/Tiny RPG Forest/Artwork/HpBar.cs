38de69dec8c3dadca8ea036bfeca677d7f22e7bc
LTH915 <xogns0915@gmail.com>
2021-05-06 11:14:25 +0900

    Add BattleScene HpBar

---
+++ MountainTale/Assets/Tiny RPG Forest/Artwork/HpBar.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HpBar : MonoBehaviour
+{
+    [SerializeField] private GameObject healthpoint;
+    
+    void Start()
+    {
+        healthpoint.transform.localScale = new Vector3(0.5f, 1f);
+    }
+
+    void SetHp(float hpNormalized)
+    {
+        healthpoint.transform.localScale = new Vector3(hpNormalized, 1f);
+    }
+}
